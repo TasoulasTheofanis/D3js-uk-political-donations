45dda09b35204daae7d8871b03807db99df7464f
Tasoulas Theofanis <p15taso@ionio.gr>
2018-03-09 12:14:20 +0200

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -446,6 +446,6 @@ $(document).ready(function() {
       var id = d3.select(this).attr("id");
       return transition(id);
     });
-    return d3.csv("data/7500up.csv", display);
+    return d3.csv("data/BloodDonation.csv", display);
 	
 });
