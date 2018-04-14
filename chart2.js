05d412da15cddb52d1efa70de1502f66ea9d0b73
Tasoulas Theofanis <p15taso@ionio.gr>
2018-04-14 09:46:49 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -423,7 +423,7 @@ function mouseover(d, i) {
   	img.width = 42;
   	img.height = 42;
 	//document.write(img);
-	document.getElementById('img').style.left="500";
+	document.getElement(img).style.left="500";
 	document.body.appendChild(img);
 	
 /* Paradoteo 1: i create a new message that will be narrated, when someone goes over any circle*/
