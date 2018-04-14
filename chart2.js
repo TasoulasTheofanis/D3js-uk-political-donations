f7f18194dba4255906ffd1e4e2162bb15581b7d9
Tasoulas Theofanis <p15taso@ionio.gr>
2018-04-14 09:38:42 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -422,8 +422,9 @@ function mouseover(d, i) {
   	img.src = "https://raw.githubusercontent.com/ioniodi/D3js-uk-political-donations/master/photos/" + donor + ".ico";
   	img.width = 42;
   	img.height = 42;
+	img.style("left", 750 + "px");
 	//document.write(img);
-	document.body.appendChild(img).style("top", 750 + "px");
+	document.body.appendChild(img);
 	
 /* Paradoteo 1: i create a new message that will be narrated, when someone goes over any circle*/
 	var msg = new SpeechSynthesisUtterance("The donator is " + donor + " and the amount he gave is " + amount + " british pounds");
