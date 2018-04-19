6945834f3fe8ff0fa0a7fa2e3932ce8d7c2d49ed
Tasoulas Theofanis <p15taso@ionio.gr>
2018-04-19 05:29:12 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -20,9 +20,9 @@ var entityCentres = {
     company: {x: w / 3.65, y: h / 2.3},
 		0: {x: w / 3.65, y: h / 1.8},
 		other: {x: w / 1.15, y: h / 1.9},
-		society: {x: w / 1.12, y: h  / 3.2 },
-		pub: {x: w / 1.8, y: h / 2.8},
-		individual: {x: w / 3.65, y: h / 3.3}                    /* i deleted a comma*/
+		3434 : {x: w / 1.12, y: h  / 3.2 },
+		2276: {x: w / 1.8, y: h / 2.8},
+		39: {x: w / 3.65, y: h / 3.3}                    /* i deleted a comma*/
 	};
 /*paradoteo 1: coloring the circles of Labour Party, Conservative Party and Liberal Democrats*/
 var fill = d3.scale.ordinal().range(["#145506", "#100345", "#ff2200"]);  
