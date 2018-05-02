46d3b684721aec8e30218a98a7e5ef77f2157fd8
Tasoulas Theofanis <p15taso@ionio.gr>
2018-05-02 17:09:56 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -381,8 +381,10 @@ function mouseover(d, i) {
 	//paradoteo 2 doesn't work yet
 	var img = document.createElement("img");
 	img.src = "https://raw.githubusercontent.com/ioniodi/D3js-uk-political-donations/master/photos/" + donor + ".ico";
+	img.width = 42;
+	img.height = 42;
 	document.body.appendChild(img);
-	
+
 	
 	
 	// tooltip popup
